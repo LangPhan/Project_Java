a0c7b5becc5615be504c46a235e12117548ab6d8
Hong <hongnguyen060895@gmail.com>
2022-11-16 21:24:35 +0700

    haha

--- src/main/java/com/controllers/AdminController.java
+++ src/main/java/com/controllers/AdminController.java
@@ -2,26 +2,39 @@ package com.controllers;
 
 import com.models.Account;
 import com.repositories.AccountRepository;
+import com.services.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private AccountRepository accountRepository;
+    private AccountService accountService;
     private Account getLoggedUser(Principal principal){
         return accountRepository.findByUsername(principal.getName()).get();
     }
 
     @GetMapping("/")
-    public String getRegister(Model model){
-        Account account = new Account();
-        model.addAttribute(account);
+    public String getAdmin(Model model){
+        return "admin/index";
+    }
+
+    @GetMapping("/category")
+    public String getCategory(Model model){
         return "admin/category";
+    }
+
+    @GetMapping("/userlist")
+    public String getUsers(Model model){
+        List<Account> accounts = accountService.findAllAccount();
+        model.addAttribute("accountList",accounts);
+        return "admin/users";
     }
 }
