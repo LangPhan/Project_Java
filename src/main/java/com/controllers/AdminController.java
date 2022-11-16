d78a67536b8e5465061548fd0ee10a337e0189ce
Hong <hongnguyen060895@gmail.com>
2022-11-16 21:49:35 +0700

    haha

--- src/main/java/com/controllers/AdminController.java
+++ src/main/java/com/controllers/AdminController.java
@@ -3,19 +3,26 @@ package com.controllers;
 import com.models.Account;
 import com.repositories.AccountRepository;
 import com.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
     private AccountRepository accountRepository;
+    @Autowired
     private AccountService accountService;
     private Account getLoggedUser(Principal principal){
         return accountRepository.findByUsername(principal.getName()).get();
@@ -37,4 +44,17 @@ public class AdminController {
         model.addAttribute("accountList",accounts);
         return "admin/users";
     }
+
+    @GetMapping("/userlist/edit/{id}")
+    public ModelAndView gettingEdit(@PathVariable(name= "id") int id){
+        ModelAndView mav = new ModelAndView("admin/edituser");
+        Optional<Account> user = accountService.findAccountById(id);
+        mav.addObject("user", user);
+        return mav;
+    }
+//    @PostMapping("/edit/{id}")
+//    public String postingEdit(@ModelAttribute("employee") Employee employee){
+//        service.save(employee);
+//        return "redirect:/employees/";
+//    }
 }
--- src/main/java/com/services/AccountService.java
+++ src/main/java/com/services/AccountService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -16,5 +17,9 @@ public class AccountService {
 
     public List<Account> findAllAccount(){
         return accountRepository.findAll();
+    }
+
+    public Optional<Account> findAccountById(long id){
+        return accountRepository.findById(id);
     }
 }
